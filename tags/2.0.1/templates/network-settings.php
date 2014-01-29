b088b7f96ec581ba7ac57a5ea37989e06fbae5a0
jokr <jokr@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-29 16:05:21 +0000

    Release 2.0.1
    git-svn-id: https://plugins.svn.wordpress.org/network-summary@847739 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/2.0.1/templates/network-settings.php
@@ -0,0 +1,52 @@
+<?php
+if ( ! current_user_can( 'manage_network' ) ) {
+	wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+}
+
+if ( isset($sites_table) ) :
+	global $network_summary;
+	$deciding_role = $network_summary->get_option( 'deciding_role', true );
+
+	if ( isset($_GET['updated']) ) {
+		?>
+		<div id="message" class="updated"><p><strong><?php _e( 'Options saved.' ) ?></strong></p></div><?php
+	}
+	?>
+
+	<div class="wrap">
+		<h2><?php _e( 'Network Summary Settings', 'network-summary' ) ?></h2>
+
+		<p><?php _e( 'The following setting allow you to define whether each site administrator is able to decide on their own,
+			whether their contents are visible to other sites or only network administrators should be able to decide this.
+			Changing this setting has no influence on the current settings of each site.', 'network-summary' ); ?></p>
+	</div>
+	<form method="post"
+	      action="<?php echo admin_url( 'admin-post.php?action=update_network_summary_network_settings' ); ?>">
+		<?php wp_nonce_field( 'network_summary_settings' ); ?>
+		<table class="form-table">
+			<tbody>
+			<tr>
+				<th class="label-row-deciding-role"><label
+						for="<?php echo NETWORK_SUMMARY_OPTIONS ?>[deciding_role]"><?php _e( 'Deciding
+						Role on Sharing Content', 'network-summary' ); ?></label></th>
+				<td>
+
+					<select name="<?php echo NETWORK_SUMMARY_OPTIONS ?>[deciding_role]" required>
+						<option <?php selected( $deciding_role, 'site_admin', true ) ?>
+							value="site_admin">
+							<?php _e( 'Site Admin', 'network-summary' ); ?>
+						</option>
+						<option <?php selected( $deciding_role, 'network_admin', true ) ?>
+							value="network_admin"><?php _e( 'Network Admin', 'network-summary' ); ?>
+						</option>
+					</select>
+				</td>
+			</tr>
+			</tbody>
+		</table>
+
+		<h3><?php echo __( 'Site Sharing Settings' ) ?></h3>
+		<?php $sites_table->display(); ?>
+		<?php submit_button(); ?>
+	</form>
+<?php endif;
