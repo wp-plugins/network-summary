a187372129dd10349780df53ab8e8f010270cf8f
jokr <jokr@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-21 16:41:29 +0000

    wait for release a bit
    git-svn-id: https://plugins.svn.wordpress.org/network-summary/trunk@862448 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- network-summary.php
+++ network-summary.php
@@ -3,7 +3,7 @@
 Plugin Name: Network Summary
 Plugin URI: http://www.aleaiactaest.ch/network-summary
 Description: Allows the display of content from other sites within the same network.
-Version: 2.0.5
+Version: 2.0.4
 Network: True
 Author: Joel Krebs
 Author URI: http://www.aleaiactaest.ch
