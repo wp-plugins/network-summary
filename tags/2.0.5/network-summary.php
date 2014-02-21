585cbdfd35ad96652c1f68a72e2cd4c8c08f3db4
jokr <jokr@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-21 19:49:36 +0000

    = 2.0.5 = * Fixed permalink structure in rss feed. * Flushing rewrite rules at the correct moment. * Adding minposts parameter. * Added a custom rss feed builder.
    git-svn-id: https://plugins.svn.wordpress.org/network-summary@862563 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/2.0.5/network-summary.php
@@ -0,0 +1,57 @@
+<?php
+/*
+Plugin Name: Network Summary
+Plugin URI: http://www.aleaiactaest.ch/network-summary
+Description: Allows the display of content from other sites within the same network.
+Version: 2.0.5
+Network: True
+Author: Joel Krebs
+Author URI: http://www.aleaiactaest.ch
+License: GPL2
+*/
+
+define( 'NETWORK_SUMMARY_DIR', plugin_dir_path( __FILE__ ) );
+define( 'NETWORK_SUMMARY_URL', plugin_dir_url( __FILE__ ) );
+
+include_once dirname( __FILE__ ) . '/includes/class-network-summary.php';
+
+if ( class_exists( 'Network_Summary' ) ) {
+	$network_summary = new Network_Summary();
+
+	register_activation_hook( __FILE__, array( $network_summary, 'activate' ) );
+	register_deactivation_hook( __FILE__, array( $network_summary, 'deactivate' ) );
+
+	function get_site_categories( $id = null ) {
+		if ( ! isset( $id ) ) {
+			$id = get_current_blog_id();
+		}
+		global $network_summary;
+		return $network_summary->get_site_categories( $id );
+	}
+
+	function set_site_category( $id, $category ) {
+		global $network_summary;
+		$network_summary->set_site_category( $id, $category );
+	}
+
+	function remove_site_category( $id, $category ) {
+		global $network_summary;
+		$network_summary->remove_site_category( $id, $category );
+	}
+
+	function share_site( $id = null ) {
+		if ( ! isset( $id ) ) {
+			$id = get_current_blog_id();
+		}
+		$option = get_blog_option( $id, Network_Summary::site_option );
+		return $option['share_site'];
+	}
+
+	function site_description( $id = null ) {
+		if ( ! isset( $id ) ) {
+			$id = get_current_blog_id();
+		}
+		$option = get_blog_option( $id, Network_Summary::site_option );
+		return $option['site_description'];
+	}
+}
