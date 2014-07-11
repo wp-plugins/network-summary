1d3d4c97ea5f6ac1320cebbf5e118fe5e5ea8c21
jokr <jokr@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-11 13:07:22 +0000

    Release of 2.0.8
    git-svn-id: https://plugins.svn.wordpress.org/network-summary@946877 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/2.0.8/includes/class-network-summary-shortcode.php
@@ -0,0 +1,101 @@
+<?php
+
+abstract class Network_Summary_Shortcode
+{
+	protected $args;
+	protected $args_hash;
+	protected $cached;
+	protected $output;
+	protected $errors;
+	protected $plugin;
+	protected $caching;
+
+	public function __construct( Network_Summary $network_summary, $atts, $caching = true ) {
+		$this->plugin = $network_summary;
+
+		$this->parse_and_validate_args( $atts );
+
+		$this->caching = $caching;
+		$this->args_hash = $this->get_hash();
+		if ( $caching AND $cache = get_transient( $this->get_transient_handle() . $this->args_hash ) ) {
+			$this->cached = true;
+			$this->output = $cache;
+		} else {
+			$this->cached = false;
+		}
+	}
+
+	protected abstract function get_transient_handle();
+
+	public function output() {
+		$this->enqueue_styles();
+		if ( empty( $this->errors ) ) {
+			if ( ! $this->cached ) {
+				$this->output = $this->generate_output();
+			}
+		} else {
+			$this->output = '<ul class="error">';
+			foreach ( $this->errors as $error ) {
+				$this->output .= '<li>' . $error . '</li>';
+			}
+			$this->output .= '</ul>';
+		}
+		if ( $this->caching ) {
+			set_transient( 'netview_overview_' . $this->args_hash, $this->output, 7200 );
+		}
+		return $this->output;
+	}
+
+	protected function parse_and_validate_args( $atts ) {
+		$this->parse_args( $atts );
+		$this->validate_args();
+	}
+
+	protected function get_hash() {
+		return md5( serialize( $this->args ) );
+	}
+
+	protected function enqueue_styles() {
+		wp_enqueue_style( 'network_summary' );
+	}
+
+	protected abstract function generate_output();
+
+	protected abstract function parse_args( $atts );
+
+	protected abstract function validate_args();
+
+	protected function extract_boolVal( $var ) {
+		switch ( $var ) {
+			case $var === true:
+			case $var == 1:
+			case strtolower( $var ) == 'true':
+			case strtolower( $var ) == 'on':
+			case strtolower( $var ) == 'yes':
+			case strtolower( $var ) == 'y':
+				$out = true;
+				break;
+			default:
+				$out = false;
+		}
+		return $out;
+	}
+
+	protected function get_recent_posts( $number_of_posts, $dateFormat ) {
+		$result = '<ul class="site-recent-post">';
+
+		$recent_posts = wp_get_recent_posts( array( 'numberposts' => $number_of_posts, 'post_status' => 'publish' ) );
+		foreach ( $recent_posts as $post ) {
+			$result .= '<li><a href="' . get_permalink( $post["ID"] ) . '" title="Read ' . $post["post_title"] . '.">'
+				. $post["post_title"] . '</a><span class="netview-date">'
+				. date_i18n( $dateFormat, strtotime( $post["post_date"] ) )
+				. '</span></li>';
+		}
+		$result .= '</ul>';
+		return $result;
+	}
+
+	protected function sort_sites_by_name( $site_a, $site_b ) {
+		return strcmp( get_blog_option( $site_b, 'blogname' ), get_blog_option( $site_a, 'blogname' ) );
+	}
+}
---
+++ tags/2.0.8/templates/feed-rss2-network.php
@@ -0,0 +1,167 @@
+<?php
+/**
+ * RSS2 Feed Template for displaying RSS2 Posts feed for all visible sites in the network.
+ *
+ */
+
+global $network_summary;
+
+$sites = array();
+
+if ( isset( $_GET['sites'] ) ) {
+	$sites = $_GET['sites'];
+}
+
+if ( isset( $_GET['category'] ) ) {
+	if ( is_array( $_GET['category'] ) ) {
+		foreach ( $_GET['category'] as $category ) {
+			$sites = array_merge( $sites, $network_summary->get_sites_per_category( $category ) );
+		}
+	} else {
+		$sites = $network_summary->get_sites_per_category( $_GET['category'] );
+	}
+}
+
+if ( empty( $sites ) ) {
+	$sites = $network_summary->get_sites();
+}
+
+header( 'Content-Type: ' . feed_content_type( 'rss-http' ) . '; charset=' . get_option( 'blog_charset' ), true );
+$more = 1;
+
+if ( ! function_exists( 'siteinfo_rss' ) ) {
+	function siteinfo_rss( $show = '' ) {
+		echo convert_chars( strip_tags( get_site_option( $show ) ) );
+	}
+}
+
+echo '<?xml version="1.0" encoding="' . get_option( 'blog_charset' ) . '"?' . '>'; ?>
+
+<rss version="2.0"
+	 xmlns:content="http://purl.org/rss/1.0/modules/content/"
+	 xmlns:wfw="http://wellformedweb.org/CommentAPI/"
+	 xmlns:dc="http://purl.org/dc/elements/1.1/"
+	 xmlns:atom="http://www.w3.org/2005/Atom"
+	 xmlns:sy="http://purl.org/rss/1.0/modules/syndication/"
+	 xmlns:slash="http://purl.org/rss/1.0/modules/slash/"
+	<?php
+	/**
+	 * Fires at the end of the RSS root to add namespaces.
+	 *
+	 * @since 2.0.0
+	 */
+	do_action( 'rss2_ns' );
+	?>
+	>
+
+	<channel>
+		<title><?php siteinfo_rss( 'site_name' ); ?></title>
+		<atom:link href="<?php self_link(); ?>" rel="self" type="application/rss+xml"/>
+		<link><?php siteinfo_rss( 'siteurl' ); ?></link>
+		<lastBuildDate><?php echo mysql2date( 'D, d M Y H:i:s +0000', get_lastpostmodified( 'GMT' ), false ); ?></lastBuildDate>
+		<language><?php bloginfo_rss( 'language' ); ?></language>
+		<?php
+		/**
+		 * Filter how often to update the RSS feed.
+		 *
+		 * @since 2.1.0
+		 *
+		 * @param string $duration The update period.
+		 *                         Default 'hourly'. Accepts 'hourly', 'daily', 'weekly', 'monthly', 'yearly'.
+		 */
+		?>
+		<sy:updatePeriod><?php echo apply_filters( 'rss_update_period', 'hourly' ); ?></sy:updatePeriod>
+		<?php
+		/**
+		 * Filter the RSS update frequency.
+		 *
+		 * @since 2.1.0
+		 *
+		 * @param string $frequency An integer passed as a string representing the frequency
+		 *                          of RSS updates within the update period. Default '1'.
+		 */
+		?>
+		<sy:updateFrequency><?php echo apply_filters( 'rss_update_frequency', '1' ); ?></sy:updateFrequency>
+		<description><?php _e( sprintf( 'Aggregated RSS Feed for %s.', get_site_option( 'site_name' ) ), 'network-summary' ); ?></description>
+		<?php
+		/**
+		 * Fires at the end of the RSS2 Feed Header.
+		 *
+		 * @since 2.0.0
+		 */
+		do_action( 'rss2_head' );
+
+		/*
+		 * Fetches all the posts.
+		 */
+		$posts = array();
+		foreach ( $sites as $site_id ) {
+			if ( share_site( $site_id ) ) {
+				switch_to_blog( $site_id );
+				$query = new WP_Query();
+				foreach ( $query->get_posts() as $post ) {
+					$post->site_id = $site_id;
+					array_push( $posts, $post );
+				}
+				restore_current_blog();
+			}
+		}
+
+		/*
+		 * Sort the posts.
+		 */
+		function sort_by_post_date( $a, $b ) {
+			return strtotime( $b->post_date_gmt ) - strtotime( $a->post_date_gmt );
+		}
+
+		usort( $posts, 'sort_by_post_date' );
+
+		$limit = get_site_option( Network_Summary::network_option );
+		$limit = $limit['rss_limit'];
+		$posts = array_slice( $posts, 0, $limit );
+
+		/*
+		 * Outputs all the posts.
+		 */
+		foreach ( $posts as $post ) :
+			switch_to_blog( $post->site_id );
+			?>
+			<item>
+				<title><?php the_title_rss() ?></title>
+				<link><?php the_permalink_rss() ?></link>
+				<comments><?php comments_link_feed(); ?></comments>
+				<pubDate><?php echo mysql2date( 'D, d M Y H:i:s +0000', get_post_time( 'Y-m-d H:i:s', true ), false ); ?></pubDate>
+				<dc:creator><![CDATA[<?php bloginfo( 'name' ); ?>]]></dc:creator>
+				<?php the_category_rss( 'rss2' ) ?>
+
+				<guid isPermaLink="false"><?php the_guid(); ?></guid>
+				<?php if ( get_option( 'rss_use_excerpt' ) ) : ?>
+					<description><![CDATA[<?php the_excerpt_rss(); ?>]]></description>
+				<?php else : ?>
+					<description><![CDATA[<?php the_excerpt_rss(); ?>]]></description>
+					<?php $content = get_the_content_feed( 'rss2' ); ?>
+					<?php if ( strlen( $content ) > 0 ) : ?>
+						<content:encoded><![CDATA[<?php echo $content; ?>]]></content:encoded>
+					<?php else : ?>
+						<content:encoded><![CDATA[<?php the_excerpt_rss(); ?>]]></content:encoded>
+					<?php endif; ?>
+				<?php endif; ?>
+				<wfw:commentRss><?php echo esc_url( untrailingslashit( get_permalink() ) . get_post_comments_feed_link( null, 'rss2' ) ); ?></wfw:commentRss>
+				<slash:comments><?php echo get_comments_number(); ?></slash:comments>
+				<?php rss_enclosure(); ?>
+				<?php
+				/**
+				 * Fires at the end of each RSS2 feed item.
+				 *
+				 * @since 2.0.0
+				 */
+				do_action( 'rss2_item' );
+				?>
+			</item>
+			<?php
+			restore_current_blog();
+		endforeach;
+
+		?>
+	</channel>
+</rss>
