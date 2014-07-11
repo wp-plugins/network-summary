4465ee8b770cbec0f153e9bdbfbd95765e05df43
jokr <jokr@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-11 13:04:32 +0000

    = 2.0.8 = * Tested compatibility with WordPress Core 3.9.1. * Fixed bug that did not display sharing option on individual blog correctly.
    git-svn-id: https://plugins.svn.wordpress.org/network-summary/trunk@946875 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- network-summary.php
+++ network-summary.php
@@ -3,7 +3,7 @@
 Plugin Name: Network Summary
 Plugin URI: http://www.aleaiactaest.ch/network-summary
 Description: Allows the display of content from other sites within the same network.
-Version: 2.0.7
+Version: 2.0.8
 Network: True
 Author: Joel Krebs
 Author URI: http://www.aleaiactaest.ch
